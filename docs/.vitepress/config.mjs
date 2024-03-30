0b0a223b1f670ac066041fd07c6aabffc30c442f
Manuel Lara <manuellaraa@gmail.com>
2024-03-29 19:10:12 -0700

    sidebar change

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -88,7 +88,7 @@ export default defineConfig({
           },
           {
             text: "Submit",
-            link: "/product/application",
+            link: "/product/submit",
           },
         ],
       },
